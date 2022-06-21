39d370d046995a45e2a01b5d2147509920214dc5
Shivam Chaudhary <shivamc021999@gmail.com>
2022-06-21 22:06:25 +0530

    modified the code for in line code editing and added a break of 1 minute after each block

--- test-real-time_lastrun.py
+++ test-real-time_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.1.3),
-    on May 29, 2022, at 11:56
+    on June 21, 2022, at 21:56
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -27,6 +27,16 @@ import sys  # to get file system encoding
 import psychopy.iohub as io
 from psychopy.hardware import keyboard
 
+from pylsl import StreamInfo, StreamOutlet
+
+info = StreamInfo('DrumTrackMarkerStream', 'Markers', 1, 0, 'string', 'myuidw43536')
+
+# next make an outlet
+outlet = StreamOutlet(info)
+
+print("sending drum track markers...")
+
+outlet.push_sample(["ExptBeg"])
 
 
 # Ensure that relative paths start from the same directory as this script
@@ -49,7 +59,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='E:\\studies\\college\\IIT Gandhinagar\\SEM III\\Internship\\Experimental Design\\PsychopyTest\\test-real-time_lastrun.py',
+    originPath='E:\\studies\\college\\IIT Gandhinagar\\SEM III\\Internship\\Experimental Design\\Drumming-EEG-Expt-Psychopy\\test-real-time_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
@@ -63,7 +73,7 @@ frameTolerance = 0.001  # how close to onset before 'same' frame
 
 # Setup the Window
 win = visual.Window(
-    size=[1280, 720], fullscr=False, screen=0, 
+    size=[1536, 864], fullscr=False, screen=0, 
     winType='pyglet', allowGUI=True, allowStencil=False,
     monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
     blendMode='avg', useFBO=True, 
@@ -92,7 +102,7 @@ defaultKeyboard = keyboard.Keyboard(backend='iohub')
 # Initialize components for Routine "WelcomeScreen"
 WelcomeScreenClock = core.Clock()
 Welcome = visual.TextStim(win=win, name='Welcome',
-    text='Welcome to the experiment!\nPress SPACE to continue.',
+    text='Welcome to this experiment!\nPress SPACE to continue.',
     font='Open Sans',
     pos=(0, 0), height=0.05, wrapWidth=None, ori=0.0, 
     color='white', colorSpace='rgb', opacity=None, 
@@ -103,7 +113,7 @@ key_resp = keyboard.Keyboard()
 # Initialize components for Routine "InstructionScreen"
 InstructionScreenClock = core.Clock()
 Instruction = visual.TextStim(win=win, name='Instruction',
-    text='Your task is to listen to the played music and respond to the questions shown. The exact procedure is as follows.\n\n1. 5 to 10 seconds of silence, focus on the white cross on the screen, and relax.\n\n2. Listen to the music played in a relaxed state. Try not to make any body movements except natural blinking.\n\n3. Respond to the questions after each music listening trial, the answer should be according to the previously played track.\n\nRemember to focus on the white cross in the center of the screen at all times, and avoid head, finger, limb, and torso movements (no need to worry if you did move).\n\nRelax, breathe normally, clarify doubts if any, and press SPACE to continue.',
+    text='Your task is to listen to the played music and respond to the questions shown. The exact procedure is as follows.\n\n1. 10 seconds of silence, focus on the white cross on the screen, and relax.\n\n2. Listen to the music played in a relaxed state. Try not to make any body movements except natural eye blinks.\n\n3. Respond to the questions after each music listening trial, the answer should be according to the previously played track.\n\nRemember to focus on the white cross in the center of the screen at all times, and avoid head, finger, limb, and torso movements (no need to worry if you did move).\n\nRelax, breathe normally, clarify doubts if any, and press SPACE to continue.',
     font='Open Sans',
     pos=(0, 0), height=0.04, wrapWidth=None, ori=0.0, 
     color='white', colorSpace='rgb', opacity=None, 
@@ -134,16 +144,27 @@ DrumTrack = sound.Sound('A', secs=-1, stereo=True, hamming=True,
     name='DrumTrack')
 DrumTrack.setVolume(1.0)
 
-# Initialize components for Routine "EnjoymentRating"
-EnjoymentRatingClock = core.Clock()
+# Initialize components for Routine "PleasantnessRating"
+PleasantnessRatingClock = core.Clock()
 KeyboardResponse = visual.TextStim(win=win, name='KeyboardResponse',
-    text='Please enter your enjoyment rating of the song from 0 to 9.',
+    text='Please enter the pleasantness \nrating of the song from 0 to 9.',
     font='Open Sans',
     pos=(0, 0), height=0.05, wrapWidth=None, ori=0.0, 
     color='white', colorSpace='rgb', opacity=None, 
     languageStyle='LTR',
     depth=0.0);
 key_resp_3 = keyboard.Keyboard()
+
+# Initialize components for Routine "Break"
+BreakClock = core.Clock()
+text = visual.TextStim(win=win, name='text',
+    text='Please take a 1 minute break.\nPress SPACE after the break to continue.\n',
+    font='Open Sans',
+    pos=(0, 0), height=0.05, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+break_end = keyboard.Keyboard()
 
 # Initialize components for Routine "FinalScreen"
 FinalScreenClock = core.Clock()
@@ -366,7 +387,7 @@ thisExp.nextEntry()
 routineTimer.reset()
 
 # set up handler to look after randomisation of conditions etc
-trials = data.TrialHandler(nReps=1.0, method='random', 
+trials = data.TrialHandler(nReps=3.0, method='random', 
     extraInfo=expInfo, originPath=-1,
     trialList=data.importConditions('Stimulus/stimuli.csv'),
     seed=None, name='trials')
@@ -386,9 +407,10 @@ for thisTrial in trials:
     
     # ------Prepare to start Routine "StimulusFixation"-------
     continueRoutine = True
-    routineTimer.add(5.000000)
+    routineTimer.add(10.000000)
     # update component parameters for each repeat
     PreTrialFixation.setText('+')
+    outlet.push_sample(["FixBeg-" + TrackName])
     # keep track of which components have finished
     StimulusFixationComponents = [PreTrialFixation]
     for thisComponent in StimulusFixationComponents:
@@ -423,7 +445,7 @@ for thisTrial in trials:
             PreTrialFixation.setAutoDraw(True)
         if PreTrialFixation.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > PreTrialFixation.tStartRefresh + 5-frameTolerance:
+            if tThisFlipGlobal > PreTrialFixation.tStartRefresh + 10-frameTolerance:
                 # keep track of stop time/frame for later
                 PreTrialFixation.tStop = t  # not accounting for scr refresh
                 PreTrialFixation.frameNStop = frameN  # exact frame index
@@ -453,13 +475,15 @@ for thisTrial in trials:
             thisComponent.setAutoDraw(False)
     trials.addData('PreTrialFixation.started', PreTrialFixation.tStartRefresh)
     trials.addData('PreTrialFixation.stopped', PreTrialFixation.tStopRefresh)
+    outlet.push_sample(["FixEnd-" + TrackName])
     
     # ------Prepare to start Routine "AudioAndFixation"-------
     continueRoutine = True
-    routineTimer.add(10.000000)
+    routineTimer.add(30.000000)
     # update component parameters for each repeat
-    DrumTrack.setSound(TrackPath, secs=10, hamming=True)
+    DrumTrack.setSound(TrackPath, secs=30, hamming=True)
     DrumTrack.setVolume(1.0, log=False)
+    outlet.push_sample(["AudBeg-" + TrackName])
     # keep track of which components have finished
     AudioAndFixationComponents = [DuringTrialFixation, DrumTrack]
     for thisComponent in AudioAndFixationComponents:
@@ -494,7 +518,7 @@ for thisTrial in trials:
             DuringTrialFixation.setAutoDraw(True)
         if DuringTrialFixation.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > DuringTrialFixation.tStartRefresh + 10-frameTolerance:
+            if tThisFlipGlobal > DuringTrialFixation.tStartRefresh + 30-frameTolerance:
                 # keep track of stop time/frame for later
                 DuringTrialFixation.tStop = t  # not accounting for scr refresh
                 DuringTrialFixation.frameNStop = frameN  # exact frame index
@@ -509,7 +533,7 @@ for thisTrial in trials:
             DrumTrack.play()  # start the sound (it finishes automatically)
         if DrumTrack.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > DrumTrack.tStartRefresh + 10-frameTolerance:
+            if tThisFlipGlobal > DrumTrack.tStartRefresh + 30-frameTolerance:
                 # keep track of stop time/frame for later
                 DrumTrack.tStop = t  # not accounting for scr refresh
                 DrumTrack.frameNStop = frameN  # exact frame index
@@ -542,16 +566,17 @@ for thisTrial in trials:
     DrumTrack.stop()  # ensure sound has stopped at end of routine
     trials.addData('DrumTrack.started', DrumTrack.tStart)
     trials.addData('DrumTrack.stopped', DrumTrack.tStop)
+    outlet.push_sample(["AudEnd-" + TrackName])
     
-    # ------Prepare to start Routine "EnjoymentRating"-------
+    # ------Prepare to start Routine "PleasantnessRating"-------
     continueRoutine = True
     # update component parameters for each repeat
     key_resp_3.keys = []
     key_resp_3.rt = []
     _key_resp_3_allKeys = []
     # keep track of which components have finished
-    EnjoymentRatingComponents = [KeyboardResponse, key_resp_3]
-    for thisComponent in EnjoymentRatingComponents:
+    PleasantnessRatingComponents = [KeyboardResponse, key_resp_3]
+    for thisComponent in PleasantnessRatingComponents:
         thisComponent.tStart = None
         thisComponent.tStop = None
         thisComponent.tStartRefresh = None
@@ -561,14 +586,14 @@ for thisTrial in trials:
     # reset timers
     t = 0
     _timeToFirstFrame = win.getFutureFlipTime(clock="now")
-    EnjoymentRatingClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+    PleasantnessRatingClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
     frameN = -1
     
-    # -------Run Routine "EnjoymentRating"-------
+    # -------Run Routine "PleasantnessRating"-------
     while continueRoutine:
         # get current time
-        t = EnjoymentRatingClock.getTime()
-        tThisFlip = win.getFutureFlipTime(clock=EnjoymentRatingClock)
+        t = PleasantnessRatingClock.getTime()
+        tThisFlip = win.getFutureFlipTime(clock=PleasantnessRatingClock)
         tThisFlipGlobal = win.getFutureFlipTime(clock=None)
         frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
         # update/draw components on each frame
@@ -619,7 +644,7 @@ for thisTrial in trials:
         if not continueRoutine:  # a component has requested a forced-end of Routine
             break
         continueRoutine = False  # will revert to True if at least one component still running
-        for thisComponent in EnjoymentRatingComponents:
+        for thisComponent in PleasantnessRatingComponents:
             if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
                 continueRoutine = True
                 break  # at least one component has not yet finished
@@ -628,8 +653,8 @@ for thisTrial in trials:
         if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
             win.flip()
     
-    # -------Ending Routine "EnjoymentRating"-------
-    for thisComponent in EnjoymentRatingComponents:
+    # -------Ending Routine "PleasantnessRating"-------
+    for thisComponent in PleasantnessRatingComponents:
         if hasattr(thisComponent, "setAutoDraw"):
             thisComponent.setAutoDraw(False)
     trials.addData('KeyboardResponse.started', KeyboardResponse.tStartRefresh)
@@ -642,9 +667,119 @@ for thisTrial in trials:
         trials.addData('key_resp_3.rt', key_resp_3.rt)
     trials.addData('key_resp_3.started', key_resp_3.tStartRefresh)
     trials.addData('key_resp_3.stopped', key_resp_3.tStopRefresh)
-    # the Routine "EnjoymentRating" was not non-slip safe, so reset the non-slip timer
+    if theseKeys != []:
+        for thisKey in theseKeys:
+            outlet.push_sample(["Rating-"+ thisKey.name +"-"+ TrackName])
+    # the Routine "PleasantnessRating" was not non-slip safe, so reset the non-slip timer
     routineTimer.reset()
-# completed 1.0 repeats of 'trials'
+    
+    # ------Prepare to start Routine "Break"-------
+    continueRoutine = True
+    # update component parameters for each repeat
+    break_end.keys = []
+    break_end.rt = []
+    _break_end_allKeys = []
+    if trials.thisN == 13:
+        continueRoutine = True
+        outlet.push_sample(["BreakBeg"])
+    else:
+        continueRoutine = False
+    # keep track of which components have finished
+    BreakComponents = [text, break_end]
+    for thisComponent in BreakComponents:
+        thisComponent.tStart = None
+        thisComponent.tStop = None
+        thisComponent.tStartRefresh = None
+        thisComponent.tStopRefresh = None
+        if hasattr(thisComponent, 'status'):
+            thisComponent.status = NOT_STARTED
+    # reset timers
+    t = 0
+    _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+    BreakClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+    frameN = -1
+    
+    # -------Run Routine "Break"-------
+    while continueRoutine:
+        # get current time
+        t = BreakClock.getTime()
+        tThisFlip = win.getFutureFlipTime(clock=BreakClock)
+        tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+        # update/draw components on each frame
+        
+        # *text* updates
+        if text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+            # keep track of start time/frame for later
+            text.frameNStart = frameN  # exact frame index
+            text.tStart = t  # local t and not account for scr refresh
+            text.tStartRefresh = tThisFlipGlobal  # on global time
+            win.timeOnFlip(text, 'tStartRefresh')  # time at next scr refresh
+            text.setAutoDraw(True)
+        if text.status == STARTED:
+            if bool(0):
+                # keep track of stop time/frame for later
+                text.tStop = t  # not accounting for scr refresh
+                text.frameNStop = frameN  # exact frame index
+                win.timeOnFlip(text, 'tStopRefresh')  # time at next scr refresh
+                text.setAutoDraw(False)
+        
+        # *break_end* updates
+        waitOnFlip = False
+        if break_end.status == NOT_STARTED and tThisFlip >= 60-frameTolerance:
+            # keep track of start time/frame for later
+            break_end.frameNStart = frameN  # exact frame index
+            break_end.tStart = t  # local t and not account for scr refresh
+            break_end.tStartRefresh = tThisFlipGlobal  # on global time
+            win.timeOnFlip(break_end, 'tStartRefresh')  # time at next scr refresh
+            break_end.status = STARTED
+            # keyboard checking is just starting
+            waitOnFlip = True
+            win.callOnFlip(break_end.clock.reset)  # t=0 on next screen flip
+            win.callOnFlip(break_end.clearEvents, eventType='keyboard')  # clear events on next screen flip
+        if break_end.status == STARTED and not waitOnFlip:
+            theseKeys = break_end.getKeys(keyList=['space'], waitRelease=False)
+            _break_end_allKeys.extend(theseKeys)
+            if len(_break_end_allKeys):
+                break_end.keys = _break_end_allKeys[-1].name  # just the last key pressed
+                break_end.rt = _break_end_allKeys[-1].rt
+                # a response ends the routine
+                continueRoutine = False
+        
+        # check for quit (typically the Esc key)
+        if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+            core.quit()
+        
+        # check if all components have finished
+        if not continueRoutine:  # a component has requested a forced-end of Routine
+            break
+        continueRoutine = False  # will revert to True if at least one component still running
+        for thisComponent in BreakComponents:
+            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                continueRoutine = True
+                break  # at least one component has not yet finished
+        
+        # refresh the screen
+        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+            win.flip()
+    
+    # -------Ending Routine "Break"-------
+    for thisComponent in BreakComponents:
+        if hasattr(thisComponent, "setAutoDraw"):
+            thisComponent.setAutoDraw(False)
+    trials.addData('text.started', text.tStartRefresh)
+    trials.addData('text.stopped', text.tStopRefresh)
+    # check responses
+    if break_end.keys in ['', [], None]:  # No response was made
+        break_end.keys = None
+    trials.addData('break_end.keys',break_end.keys)
+    if break_end.keys != None:  # we had a response
+        trials.addData('break_end.rt', break_end.rt)
+    trials.addData('break_end.started', break_end.tStartRefresh)
+    trials.addData('break_end.stopped', break_end.tStopRefresh)
+    # the Routine "Break" was not non-slip safe, so reset the non-slip timer
+    routineTimer.reset()
+# completed 3.0 repeats of 'trials'
 
 
 # ------Prepare to start Routine "FinalScreen"-------
@@ -715,6 +850,7 @@ for thisComponent in FinalScreenComponents:
         thisComponent.setAutoDraw(False)
 thisExp.addData('ThankYou.started', ThankYou.tStartRefresh)
 thisExp.addData('ThankYou.stopped', ThankYou.tStopRefresh)
+outlet.push_sample(["ExptEnd"])
 
 # Flip one final time so any remaining win.callOnFlip() 
 # and win.timeOnFlip() tasks get executed before quitting
